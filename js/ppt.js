0dc0b9ea3a089ccb18b611c705bca73e45b70e73
ZH_MiSha <254350006@qq.com>
2024-07-18 00:36:00 +0800

    first commit

---
+++ js/ppt.js
@@ -0,0 +1,31 @@
+// 获取所有幻灯片元素
+const slides = document.querySelectorAll('.slide');
+
+// 定义当前幻灯片索引
+let currentSlide = 0;
+
+// 定义切换幻灯片的函数
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    });
+}
+
+// 定义滚动阈值
+const scrollThreshold = 50; // 可以根据需要调整这个值
+
+// 监听鼠标滚轮事件
+document.addEventListener('wheel', (event) => {
+    if (event.deltaY > scrollThreshold) {
+        // 向下滚动
+        if (currentSlide < slides.length - 1) {
+            currentSlide++;
+        }
+    } else if (event.deltaY < -scrollThreshold) {
+        // 向上滚动
+        if (currentSlide > 0) {
+            currentSlide--;
+        }
+    }
+    showSlide(currentSlide);
+});
